934a2dd5b250a2b8dc4f252d034207befb806f84
bzse9 <bzse9@192.168.56.1>
2021-04-15 17:52:28 +0200

    Eddigi kód egybehozása.

--- src/main/java/Controller/Valaszto_AblakController.java
+++ src/main/java/Controller/Valaszto_AblakController.java
@@ -12,6 +12,7 @@ import javafx.stage.Stage;
 import com.mycompany.jpa.MysqlCon;
 import java.sql.SQLException;
 
+
 public class Valaszto_AblakController implements Initializable {
     
     MysqlCon dbCon;
--- src/main/java/com/mycompany/jpa/MysqlCon.java
+++ src/main/java/com/mycompany/jpa/MysqlCon.java
@@ -1,3 +1,5 @@
+
+
 package com.mycompany.jpa;
 
 import java.sql.*;
